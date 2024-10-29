ee3e5796c48a709a75d11c206e7178981cc45b46
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-10-29 15:56:49 -0700

    feat: add models

---
+++ Src/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+namespace user_microservice.Src.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? DeletedAt { get; set; } = null;
+
+        public int Version { get; set; } = 1;
+    }
+}
---
+++ Src/Models/User.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace user_microservice.Src.Models
+{
+    public class User : BaseModel
+    {
+        [StringLength(250)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(250)]
+        public string FirstLastName { get; set; } = null!;
+
+        [StringLength(250)]
+        public string SecondLastName { get; set; } = null!;
+
+        [StringLength(250)]
+        public string RUT { get; set; } = null!;
+
+        [StringLength(250)]
+        public string Email { get; set; } = null!;
+
+        [StringLength(250)]
+        public string HashedPassword { get; set; } = null!;
+
+        public bool IsEnabled { get; set; } = true;
+
+
+
+        public int CareerId { get; set; }
+        public Career Career { get; set; } = null!;
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+    }
+}
