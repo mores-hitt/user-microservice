f9a85db0c77414e75bcd92571ae333128bd1c7e2
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-10-29 15:52:53 -0700

    feat: add subjects repository

---
+++ Src/Repositories/Interfaces/ISubjectsRepository.cs
@@ -0,0 +1,6 @@
+using user_microservice.Src.Models;
+
+namespace user_microservice.Src.Repositories.Interfaces
+{
+    public interface ISubjectsRepository : IGenericRepository<Subject> {}
+}
---
+++ Src/Repositories/SubjectsRepository.cs
@@ -0,0 +1,12 @@
+using user_microservice.Src.Data;
+using user_microservice.Src.Models;
+using user_microservice.Src.Repositories.Interfaces;
+
+namespace user_microservice.Src.Repositories
+{
+    public class SubjectsRepository : GenericRepository<Subject>, ISubjectsRepository
+    {
+        public SubjectsRepository(DataContext context)
+            : base(context) { }
+    }
+}
