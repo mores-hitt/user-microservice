dc870955bdd8f55db0168fcb7a0570f7a49cd091
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-10-29 19:20:18 -0700

    feat: add migrations, seeders and data context

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using user_microservice.Src.Models;
 
-namespace user-microservice.Src.Data
+namespace user_microservice.Src.Data
 {
     public class DataContext : DbContext
     {
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Career> Careers { get; set; }
+
+        public DbSet<UserProgress> UserProgresses { get; set; }
+
+        public DbSet<SubjectRelationship> SubjectRelationships { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options): base(options)
         {
         }
