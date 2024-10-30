9ea64d1e1af826b9823cb2bf6d3071577497d68f
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-10-29 19:30:59 -0700

    feat: add Subject controller, with getall and getallrelationships calls

---
+++ Src/Controllers/SubjectsController.cs
@@ -0,0 +1,45 @@
+using Grpc.Core;
+using user_microservice.Src.Protos;
+
+using user_microservice.Src.DTOs.Subjects;
+using user_microservice.Src.Services.Interfaces;
+
+
+namespace user_microservice.Src.Controllers
+{
+    public class SubjectsController : SubjectGrpc.SubjectGrpcBase
+    {
+
+        private readonly ISubjectsService _subjectsService;
+
+        public SubjectsController(ISubjectsService subjectsService)
+        {
+            _subjectsService = subjectsService;
+        }
+
+
+        public override async Task<GetAllResponse> GetAll(Empty request, ServerCallContext context)
+        {
+            var subjects = await _subjectsService.GetAll();
+
+            var response = new GetAllResponse();
+
+            response.Subjects.AddRange(subjects);
+
+            return response;
+        }
+    
+
+        public override async Task<GetAllRelationshipsResponse> GetAllRelationships(Empty request, ServerCallContext context)
+        {
+            var relationships = await _subjectsService.GetAllRelationships();
+
+            var response = new GetAllRelationshipsResponse();
+
+            response.Relationships.AddRange(relationships);
+
+            return response;
+        }
+
+    }
+}
